8e497ec0348296d6237ab5cab82c9f254a1278fb
Chinmay <dhaundiyalchinmay@gmail.com>
2026-01-15 22:40:59 +0530

    Add role selection to signup page

--- src/src/pages/Signup.jsx
+++ src/src/pages/Signup.jsx
@@ -1,36 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const Signup = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+    const [active, setActive] = useState('student');
+    const [role, setRole] = useState("");
 
+    const handleRoleSelect = (selectedRole) => {
+        setRole(selectedRole);
+        setActive(selectedRole.toLowerCase());
+        setValue("role", selectedRole, { shouldValidate: true });
+    };
+
+    const clickHandler = () => {
+        return console.log("REGITER BUTTON IS CLICKED");
+    }
     const onSubmit = (data) => {
         return console.log("THIS IS DATA:", data);
     }
+    console.log("THIS IS ROLE:", role);
   return (
     <div>
         {/* Parent div of signup from */}
-        <div className='flex flex-col items-center rounded-xl gap-y-5 p-10 bg-white h-160 w-[35%] mx-auto mt-20'>
+        <div className='flex flex-col items-center rounded-xl gap-y-5 p-10 bg-white max-h-230 w-[35%] mx-auto mt-20'>
             <div className='flex flex-col items-center gap-3'>
                 <h2 className='text-3xl font-zalando'>Create Your Account</h2>
                 <p className='text-neutral-500'>Sign up to get started!</p>
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col h-100 gap-y-5'>
-                <label for='name'>Username</label>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col max-h-260 gap-y-5'>
+                <label htmlFor='name'>Username</label>
                 <input type='text' placeholder='Enter Username' className='pl-2 border border-gray-500 rounded-md h-8.75 w-100 outline-none focus-visible:text-[#000080] focus-visible:ring-2 focus-visible:ring-indigo-400 focus-visible:shadow-indigo-400 focus-visible:shadow-lg transition-all ease-in' {...register("name", {required: "Username is Required, Please Enter Username"})}/>
                 {errors.name && <span className='text-red-600'>{errors.name.message}</span>}
                 
-                <label for='email'>Email Address</label>
+                <label htmlFor='email'>Email Address</label>
                 <input type='email' placeholder='Enter Email' className='pl-2 border border-gray-500 rounded-md h-8.75 w-100 outline-none focus-visible:text-[#000080] focus-visible:ring-2 focus-visible:ring-indigo-400 focus-visible:shadow-indigo-400 focus-visible:shadow-lg transition-all ease-in' {...register("email", {required: "Email is Required, Please Enter Email"})}/>
                 {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
 
-                <label for='password'>Password</label>
+                <label htmlFor='password'>Password</label>
                 <input type='password' placeholder='Enter Password' className='pl-2 border border-gray-500 rounded-md h-8.75 w-100 outline-none focus-visible:text-[#000080] focus-visible:ring-2 focus-visible:ring-indigo-400 focus-visible:shadow-indigo-400 focus-visible:shadow-lg transition-all ease-in' {...register("password", {required: "Password is Required, Please Enter Password"})}/>
                 {errors.password && <span className='text-red-600'>{errors.password.message}</span>}
-
-                <button type='submit' className='bg-linear-to-r from-[#e3ffe7] to-[#d9e7ff] mt-8 py-2 rounded-lg cursor-pointer hover:shadow-xl hover:shadow-indigo-200 transition-all ease hover:scale-105'>Register</button>
+                
+                <label htmlFor="role">Select a Role</label>
+                <input type='hidden' 
+                {...register("role", {required: "An Account Type is Requied, Please select a Role"})}/>
+                <span className='text-red-600'>{errors.role?.message}</span>
+                <div
+                className='flex justify-center gap-5 bg-indigo-700 py-2 pl-10 pr-10 min-w-62.5 rounded-full lg:w-50 text-green-100'>
+                    <button type='button' onClick={() => handleRoleSelect("Student")} 
+                    className={`cursor-pointer bg-[#191970] py-2 px-4 rounded-full ${active === 'student' ? "transition-all ease-in delay-75" 
+                    : "bg-transparent"}`}>Student</button>
+                    <button type='button' onClick={() => handleRoleSelect("Instructor")} 
+                    className={`cursor-pointer bg-[#191970] py-2 px-4 rounded-full ${active === 'instructor' ? "transition-all ease-in delay-75" 
+                    : "bg-transparent"}`}>Instructor</button>
+                </div>
+                <button onClick={clickHandler} type='submit' className='bg-linear-to-r from-[#e3ffe7] to-[#d9e7ff] mt-8 py-2 rounded-lg cursor-pointer hover:shadow-xl hover:shadow-indigo-200 transition-all ease hover:scale-105 delay-100'>Register</button>
             </form>
 
             <div className=' text-sm'>
